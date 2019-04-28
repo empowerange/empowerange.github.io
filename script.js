75e8ceaa36293e0782e4a733aa8672d0a4f5c57f
santoshvijapure <vijapuresantosh@gmail.com>
2019-04-28 12:54:48 +0530

    preloader added

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+
+$(window).on('load', function() { // makes sure the whole site is loaded 
+    $('#status').fadeOut(); // will first fade out the loading animation 
+    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+    $('body').delay(350).css({'overflow':'visible'});
+  })
+
+
 // navbar collapsing problem
 $(".navbar-nav li a").click(function(event) {
     if (!$(this).parent().hasClass('dropdown'))
